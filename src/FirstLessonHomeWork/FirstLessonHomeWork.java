30b401458a6a961b1e205209a472ed797c62c43e
Kataychyk <kataevro@gmail.com>
2019-04-11 16:10:30 +0300

    First lesson home work

---
+++ src/FirstLessonHomeWork/FirstLessonHomeWork.java
@@ -0,0 +1,32 @@
+package FirstLessonHomeWork;
+
+public class FirstLessonHomeWork {
+    public static void main (String[] args) {
+        int a = 5;
+        short b =23;
+        boolean c = true;
+        String d = "е";
+        double e =2.423235535545;
+        float f = 24.2f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Character.MAX_VALUE);
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Character.MIN_VALUE);
+    }
+}
---
+++ src/FirstLessonHomeWork/SecondQuestion.java
@@ -0,0 +1,12 @@
+package FirstLessonHomeWork;
+
+public class SecondQuestion {
+    public static void main (String[] args) {
+        System.out.print("___");
+        System.out.println("__");
+        System.out.println("|   |");
+        System.out.println("|   |");
+        System.out.print("---");
+        System.out.println("--");
+    }
+}
