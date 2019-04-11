ab0e2ea4d7291e10bcd7d1a7e9e40eb2b18712bd
Kataychyk <kataevro@gmail.com>
2019-04-11 19:35:31 +0300

    Lection 2

---
+++ src/Lection_2/Lesson_2.java
@@ -0,0 +1,17 @@
+package Lection_2;
+
+public class Lesson_2 {
+    public static void main(String[] args){
+       // String[] cars;
+        String[] cars = {"Volvo", "BMV", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+
+       
+
+        int [][] twoD = {
+                {1, 3, -7, 5},
+                {-12,19},
+                {0,15,-32}
+        };
+    }
+}
