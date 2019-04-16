57e820af760f44bb03bdfdef74abfcbd9892214b
Kataychyk <kataevro@gmail.com>
2019-04-17 00:14:06 +0300

    Lection 3 home work and fixes  minor fix

--- src/lection3/HomeWork.java
+++ src/lection3/HomeWork.java
@@ -4,10 +4,7 @@ package lection3;
 public class HomeWork {
     public static void main(String[] args) {
         int[] numArray = {12, 34, 3, 54, 18, 89, 43, 4, 77, 63};
-
         int n = numArray.length;
-
-
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
                 if (numArray[j - 1] > numArray[j]) {
@@ -21,5 +18,4 @@ public class HomeWork {
             System.out.print(numArray[i] + " ");
         }
     }
-
 }
