720890656898935c4b703fefcd034709e531c8ee
Kataychyk <kataevro@gmail.com>
2019-05-07 20:29:01 +0300

    counter fix

--- src/lection2/homework/HomeWork2.java
+++ src/lection2/homework/HomeWork2.java
@@ -6,26 +6,26 @@ public class HomeWork2 {
         // Вираховуємо розмір масиву
         int a = 0;
 
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             a++; // рахуємо кількість  чисел
         }
         // формуємо масив випадкових чисел
         int[] randomArr = new int[a];
         System.out.println("Розмір масиву 'randomArr' = " + a);
-        for (int i = 1, z = 0; i <= randomArr.length; i++) {
+        for (int i = 0; i <= randomArr.length-1; i++) {
 
-            randomArr[z] = (int) Math.ceil(Math.random() * 100); // записуємо в комірку випадкове число від 1 до 100
-            System.out.print(randomArr[z] + " ");
-            z++;
+            randomArr[i] = (int) Math.ceil(Math.random() * 100); // записуємо в комірку випадкове число від 1 до 100
+            System.out.print(randomArr[i] + " ");
+
         }
         System.out.println();
         // присвоюємо числу з непарним індексом 0
-        for (int i = 1, z = 0; i <= randomArr.length; i++) {
-            if (i % 2 != 0) {
-                randomArr[z] = 0;
+        for (int i = 0;  i <= randomArr.length-1; i++) {
+            if (i % 2 == 0) {
+                randomArr[i] = 0;
             }
-            System.out.print(randomArr[z] + " ");
-            z++;
+            System.out.print(randomArr[i] + " ");
+
         }
     }
 }
